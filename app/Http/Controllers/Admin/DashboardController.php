d20835f62d4f985346fc5b7fe70bf0ce5db3a3b7
Abid Raza <ari.abid@gmail.com>
2024-07-16 13:55:32 +0530

    Show posts and pages count on admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Str;
@@ -12,7 +14,18 @@ class DashboardController extends Controller
     public function __invoke(Request $request)
     {
         $user = $request->user();
+        $totalPostsCount = Post::count();
+        $publishedPostCount = Post::published()->count();
+        $totalPagesCount = Page::count();
+        $publishedPageCount = Page::published()->count();
 
-        return Inertia::render('Admin/Dashboard', []);
+        return Inertia::render('Admin/Dashboard', [
+            'stats' => [
+                'totalPostsCount' => $totalPostsCount,
+                'publishedPostCount' => $publishedPostCount,
+                'totalPagesCount' => $totalPagesCount,
+                'publishedPageCount' => $publishedPageCount
+            ]
+        ]);
     }
 }
