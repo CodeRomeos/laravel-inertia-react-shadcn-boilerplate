35c084d4ad3723ab7c0bd31b1ca4d8990a3c0ef1
Abid Raza <ari.abid@gmail.com>
2024-08-14 00:32:24 +0530

    Create WYSIWYG block

--- resources/js/Components/Puck/Blocks/Paragraph.jsx
+++ resources/js/Components/Puck/Blocks/Paragraph.jsx
@@ -6,7 +6,7 @@ import { marginFields, paddingFields } from "./CommonBlockProps";
 export const Paragraph = {
     label: "Paragraph",
     fields: {
-        content: { type: "textarea" },
+        body: { type: "textarea", autosize: true, name: 'body', id: 'body' },
         align: {
             type: "radio",
             options: [
@@ -20,17 +20,17 @@ export const Paragraph = {
         paddings: paddingFields.fields,
     },
     defaultProps: {
-        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto maiores excepturi delectus nam pariatur commodi libero, non impedit! Et labore ducimus perferendis placeat deserunt impedit fugiat. Reiciendis autem accusamus quam.",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto maiores excepturi delectus nam pariatur commodi libero, non impedit! Et labore ducimus perferendis placeat deserunt impedit fugiat. Reiciendis autem accusamus quam.",
         align: "left",
         margins: marginFields.defaultProps,
         paddings: paddingFields.defaultProps,
     },
-    render: ({ content, paddings, margins, align, puck }) => {
+    render: ({ body, paddings, margins, align, puck }) => {
         return (
             <p
                 className={`text-${align} ${paddingFields.classNames(paddings)} ${marginFields.classNames(margins)}`}
             >
-                {content}
+                {body}
             </p>
         );
     },
--- resources/js/Components/Puck/Blocks/WYSIWYG.jsx
+++ resources/js/Components/Puck/Blocks/WYSIWYG.jsx
@@ -2,34 +2,56 @@
 import React from "react";
 import { Autofield } from "@measured/puck";
 import { marginFields, paddingFields } from "./CommonBlockProps";
-import EditorInput from "@/Components/EditorInput";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/shadcn/ui/dialog";
+import SmallEditorInput from "@/Components/SmallEditorInput";
+import { Button } from "@/shadcn/ui/button";
 
 export const WYSIWYG = {
     label: "WYSIWYG",
     fields: {
-        content: {
+        wysiwygContent: {
             type: "custom",
             render: ({ name, onChange, value }) => (
-                <div className="wysiwyg-input bg-white">
-                    <EditorInput
-                        name={name}
-                        value={value}
-                        className="mt-1 block w-full text-xl h-16"
-                        onChange={onChange}
-                    />
-                </div>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className="w-full" type="button">Open Editor</Button>
+                    </DialogTrigger>
+                    <DialogContent className="max-w-[900px]">
+                        <DialogHeader>
+                            <DialogTitle>WYSIWYG Editor</DialogTitle>
+                        </DialogHeader>
+                        <div className="wysiwyg-input bg-white w-full">
+                            <SmallEditorInput
+                                key={name}
+                                name={name}
+                                id={name}
+                                value={value}
+                                className="mt-1 block w-full text-xl h-16"
+                                onChange={onChange}
+                            />
+                        </div>
+                    </DialogContent>
+                </Dialog>
             ),
         },
     },
     defaultProps: {
-        content:
+        wysiwygContent:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto maiores excepturi delectus nam pariatur commodi libero, non impedit! Et labore ducimus perferendis placeat deserunt impedit fugiat. Reiciendis autem accusamus quam.",
     },
-    render: ({ content, puck }) => {
+    render: ({ wysiwygContent, puck }) => {
         return (
             <div
                 className="wysiwyg-content"
-                dangerouslySetInnerHTML={{ __html: content }}
+                dangerouslySetInnerHTML={{ __html: wysiwygContent }}
             />
         );
     },
