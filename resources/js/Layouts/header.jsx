905fb4b24b92d32060b5b3eb141f8593a7213f88
Abid Raza <ari.abid@gmail.com>
2024-09-26 18:52:16 +0530

    Create responsive menu and mobile nav

--- resources/js/Layouts/header.jsx
+++ resources/js/Layouts/header.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import Nav from "./Nav";
+import Navbar from "./Navbar";
 import { Link } from "@inertiajs/react";
 import { Package2 } from "lucide-react";
+import MobileNavbar from "./MobileNavbar";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 bg-white flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+        <header className="container sticky top-0 bg-white flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <MobileNavbar />
             <Link
-                href="#"
+                href="/"
                 className="flex items-center gap-2 text-lg font-semibold md:text-base"
             >
                 <Package2 className="h-6 w-6" />
                 <span className="sr-only">Acme Inc</span>
             </Link>
-            <Nav />
+            <Navbar />
         </header>
     );
 }
