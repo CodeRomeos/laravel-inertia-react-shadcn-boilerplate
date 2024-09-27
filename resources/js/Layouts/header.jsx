03b03ebf0f40663e2f3df3335c55782570a6d701
Abid Raza <ari.abid@gmail.com>
2024-09-27 11:57:11 +0530

    Make the header full width

--- resources/js/Layouts/header.jsx
+++ resources/js/Layouts/header.jsx
@@ -6,16 +6,18 @@ import MobileNavbar from "./MobileNavbar";
 
 export default function Header() {
     return (
-        <header className="container sticky top-0 bg-white flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-            <MobileNavbar />
-            <Link
-                href="/"
-                className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            >
-                <Package2 className="h-6 w-6" />
-                <span className="sr-only">Acme Inc</span>
-            </Link>
-            <Navbar />
+        <header className="sticky top-0 bg-white">
+            <div className="container flex items-center gap-4 h-16">
+                <MobileNavbar />
+                <Link
+                    href="/"
+                    className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                >
+                    <Package2 className="h-6 w-6" />
+                    <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Navbar />
+            </div>
         </header>
     );
 }
