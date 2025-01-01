7ceeb66a4c1fae0fa702d3f4f25e80a710e1e460
Abid Raza <ari.abid@gmail.com>
2025-01-01 11:22:36 +0530

    Add application logo component

--- resources/js/Layouts/admin/Header.jsx
+++ resources/js/Layouts/admin/Header.jsx
@@ -5,28 +5,14 @@ import { UserNav } from "./user-nav";
 import { BellRing } from "lucide-react";
 import { usePage } from "@inertiajs/react";
 import MainSiteIconLink from "@/Components/Admin/MainSiteIconLink";
+import ApplicationLogo from "@/Components/ApplicationLogo";
 
 export default function Header() {
-    const { appName, globalSettings } = usePage().props;
     return (
         <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
             <nav className="h-14 flex items-center justify-between px-4">
                 <div className="hidden lg:block">
-                    <a
-                        href="/admin/dashboard"
-                        className="flex gap-x-2 items-center"
-                    >
-                        {globalSettings.general.app_logo && (
-                            <img
-                                src={globalSettings.general.app_logo}
-                                alt={globalSettings.general.app_logo || appName}
-                                className="h-12"
-                            />
-                        )}
-                        <p className="text-md font-semibold dark:text-white">
-                            {globalSettings.general.app_name || appName}
-                        </p>
-                    </a>
+                    <ApplicationLogo />
                 </div>
                 <div className={cn("block lg:!hidden")}>
                     <MobileSidebar />
